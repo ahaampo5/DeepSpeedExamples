ae821f7ead3da9c3e9b8c1952539c4b84ab0c1f9
ahaampo5 <ahaampo5@gmail.com>
2025-07-09 18:06:42 +0900

    [Sync] train data

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen3/multi-node/run_qwen3_0.6b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen3/multi-node/run_qwen3_0.6b.sh
@@ -3,27 +3,43 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
+export NCCL_DEBUG=INFO
+export NCCL_DEBUG_SUBSYS=ALL
+# export CUDA_LAUNCH_BLOCKING=1
+export NCCL_TIMEOUT=6000
+
+BATCH_SIZE=1
+ACCUMULATION_STEPS=2
+MAX_LENGTH=16384
+NUM_GPUS=8
+TARGET=code_logic_math_stem_table
+
+export WANDB_PROJECT="foundationModel"
+export WANDB_NAME="0.6B_b_$BATCH_SIZE*$ACCUMULATION_STEPS*$NUM_GPUS-$MAX_LENGTH-$TARGET" # 6*2=12
+export WANDB_NOTES="Smaller learning rate, more regularization."
+
 OUTPUT=$1
 ZERO_STAGE=$2
 if [ "$OUTPUT" = "" ]; then
     OUTPUT=./output_step1_qwen3_0.6b
 fi
 if [ "$ZERO_STAGE" = "" ]; then
-    ZERO_STAGE=3
+    ZERO_STAGE=1
 fi
 mkdir -p $OUTPUT
 
 deepspeed --hostfile=hostfile --num_nodes 2 main.py \
-   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+   --data_path HuggingFaceTB/smoltalk \
+   --data_name everyday-conversations \
    --data_split 2,4,4 \
    --model_name_or_path Qwen/Qwen3-0.6B \
-   --per_device_train_batch_size 4 \
-   --per_device_eval_batch_size 4 \
-   --max_seq_len 512 \
+   --per_device_train_batch_size $BATCH_SIZE \
+   --per_device_eval_batch_size $BATCH_SIZE \
+   --max_seq_len $MAX_LENGTH \
    --learning_rate 9.65e-6 \
    --weight_decay 0. \
-   --num_train_epochs 4  \
-   --gradient_accumulation_steps 1 \
+   --num_train_epochs 1  \
+   --gradient_accumulation_steps $ACCUMULATION_STEPS \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
@@ -31,4 +47,6 @@ deepspeed --hostfile=hostfile --num_nodes 2 main.py \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
    --output_dir $OUTPUT \
+   --offload \
+   --dtype bf16 \
    > $OUTPUT/training.log
--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen3/multi-node/run_qwen3_32b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen3/multi-node/run_qwen3_32b.sh
@@ -5,6 +5,18 @@
 # DeepSpeed Team
 export NCCL_DEBUG=WARN
 export NCCL_DEBUG_SUBSYS=ALL
+# export CUDA_LAUNCH_BLOCKING=1
+export NCCL_TIMEOUT=6000
+
+BATCH_SIZE=1
+ACCUMULATION_STEPS=2
+MAX_LENGTH=16384
+NUM_GPUS=8
+TARGET=code_logic_math_stem_table
+
+export WANDB_PROJECT="foundationModel"
+export WANDB_NAME="0.6B_b_$BATCH_SIZE*$ACCUMULATION_STEPS*$NUM_GPUS-$MAX_LENGTH-$TARGET" # 6*2=12
+export WANDB_NOTES="Smaller learning rate, more regularization."
 
 OUTPUT=$1
 ZERO_STAGE=$2
@@ -12,21 +24,22 @@ if [ "$OUTPUT" = "" ]; then
     OUTPUT=./output_step1_qwen3_32b
 fi
 if [ "$ZERO_STAGE" = "" ]; then
-    ZERO_STAGE=0
+    ZERO_STAGE=3
 fi
 mkdir -p $OUTPUT
 
 deepspeed --hostfile=hostfile --num_nodes 2 main.py \
-   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+   --data_path HuggingFaceTB/smoltalk \
+   --data_name everyday-conversations \
    --data_split 2,4,4 \
    --model_name_or_path Qwen/Qwen3-32B \
-   --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 1 \
-   --max_seq_len 1024 \
+   --per_device_train_batch_size $BATCH_SIZE \
+   --per_device_eval_batch_size $BATCH_SIZE \
+   --max_seq_len $MAX_LENGTH \
    --learning_rate 9.65e-6 \
    --weight_decay 0. \
-   --num_train_epochs 4  \
-   --gradient_accumulation_steps 1 \
+   --num_train_epochs 1  \
+   --gradient_accumulation_steps $ACCUMULATION_STEPS \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
@@ -34,4 +47,6 @@ deepspeed --hostfile=hostfile --num_nodes 2 main.py \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
    --output_dir $OUTPUT \
+   --offload \
+   --dtype bf16 \
    > $OUTPUT/training.log
